a05766d30cd230771eb1876197762deb277df5c9
Sang Tae Park <pastime1971@gmail.com>
2012-09-16 08:36:39 -0700

    version 2.1.0 release

---
+++ patches/3gdongle/README.txt
@@ -0,0 +1,9 @@
+---------------------
+For the 3G Dongle Mod
+---------------------
+
+Please, Go to trevd's OP for usage. All the hard parts are done...
+...But it will be frustrating if it doesn't work because you didn't read the Troubleshooting guide!
+
+http://forum.xda-developers.com/showthread.php?t=1798631
+
---
+++ patches/pdroid/aokp-jb/framework.jar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# inherit variables from the parent script by running it as ". script.sh"
+
+# a hack to skim down : remove vendor-specific RIL
+unset GARBAGE
+if [[ -n $RIL ]]; then
+	GARBAGE=($(find tmp/${FILE}.out/smali/com/android/internal/telephony/ -name "*RIL*" -a -not -name "RIL*" -a -not -name "${RIL}*"))
+else
+	GARBAGE=($(find tmp/${FILE}.out/smali/com/android/internal/telephony/ -name "*RIL*" -a -not -name "RIL*"))
+fi
+if [[ -n ${GARBAGE[@]} ]]; then
+	printtask "... remove unnecessary ${#GARBAGE[@]} files to avoid methods cap ..."
+	echo "DELETE RILS= ${GARBAGE[@]}" >> "$LOG"
+	\rm -rf ${GARBAGE[@]} >> "$LOG"
+fi
