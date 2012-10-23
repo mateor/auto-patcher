d664d83b8f6e9328c73e09bbb81ef6b15287923b
mateor <mateorod9@gmail.com>
2012-10-22 21:31:36 -0500

    Merge with 2.2 and bump

--- diff_tools/compare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for JAR in core services framework; do
-	diff -NpruEbB -x '*RIL*' $1$JAR $2$JAR > $JAR.patch
-done
--- diff_tools/mkdiff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf stock_* modded_*
-./decompile.sh $1 stock_git checkout 
-./decompile.sh $2 modded_
-./compare.sh stock_ modded_
