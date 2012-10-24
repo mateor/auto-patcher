39c6eb65494b3c8f89f9ad35fa629371eb13c0b4
mateor <mateorod9@gmail.com>
2012-10-24 15:50:03 -0500

    Fix batch.sh and fix_aapt.sh for OSX

--- batch.sh
+++ batch.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
-ROOT=$(readlink -f "`dirname $0`")
+abspath () {
+	case `uname -s` in
+	CYGWIN*)
+		echo $(cygpath -ua "$1")
+		;;
+	Darwin)
+		#[[ $(echo $1 | awk '/^\//') == $1 ]] && echo "$1" || echo "$PWD/$1"
+		[[ ${1:0:1} == "/" ]] && echo "$1" || echo "$PWD/$1"
+		;;
+	Linux)
+                echo $(readlink -f "$1")
+		;;
+	*)
+		if [[ ${1:0:1} == "/" ]]; then
+			echo "$1"
+		elif [[ ${1:0:2} == "./" ]]; then
+			echo "$PWD/${1:2}"
+		else
+			echo "$PWD/$1"
+		fi
+		;;
+	esac
+}
+
+extpath () {
+	case `uname -s` in
+	CYGWIN*)
+		echo $(cygpath -da "$1")
+		;;
+	*)
+		echo $(abspath "$1")
+		;;
+	esac
+}
+ROOT=$(abspath `dirname "$0"`)
+ROM=$(abspath "$1")
+ROMX=$(extpath "$1")
+
 
 cd "$ROOT/patch_tools"
 tar cvzf ../patch_tools.tgz *.jar updatecert.pem updatekey.pk8 update-template.zip boot cygwin mac linux
