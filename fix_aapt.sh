81e6a96bf4f96ca7ae6bc18f1a0ddcc17355ebd0
mateor <mateorod9@gmail.com>
2012-10-23 19:13:00 -0500

    Fix fix_aapt.sh

--- fix_aapt.sh
+++ fix_aapt.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-tar xvzf patch_tools.tgz
+ROOT=$(readlink -f "`dirname $0`")
+
+if [ -d patch_tools ]; then
+        cd "$ROOT/patch_tools"
+        tar xvzf ../patch_tools.tgz
+else 
+        mkdir  $ROOT/patch_tools
+        cd $ROOT/patch_tools
+        tar xvzf ../patch_tools.tgz
+fi
+
+
 chmod +x mac/aapt
 chmod +x cygwin/aapt.exe
 chmod +x linux/aapt
 
-tar cvzf patch_tools.tgz boot cygwin linux mac apktool.jar baksmali.jar signapk.jar smali.jar updatecert.pem updatekey.pk8
+tar cvzf ../patch_tools.tgz *.jar updatecert.pem updatekey.pk8 boot cygwin mac linux
+cd "$ROOT"
