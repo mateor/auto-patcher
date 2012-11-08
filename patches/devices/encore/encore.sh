a04e5cd974f479ce36d54e719be0ced26cdb38d9
mateor <mateorod9@gmail.com>
2012-11-08 17:52:06 -0600

    Improve encore.sh script
    Doesn't get any simpler than that

--- patches/devices/encore/encore.sh
+++ patches/devices/encore/encore.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-UPDIR=META-INF/com/google/android
-
-# Remove original updater-script
-\rm $UPDIR/updater-script >> "$LOG"
-
-# Move the template script in
-\cp -v patches/devices/$DID/$UPDIR/updater-script $UPDIR >> "$LOG"
-
-# add lines that were set with in the FILE list, from files.txt to UPDATE.txt
-echo "run_program(\"/sbin/busybox\", \"umount\", \"/system\");" >> UPDATE.txt
-
-# Append the list generated from files.txt to the new updater-script
-cat UPDATE.txt >> $UPDIR/updater-script
+replace_updater
