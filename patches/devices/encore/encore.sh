d1bedf53f91664c3f2be5e6dec7438ed118564bc
mateor <mateorod9@gmail.com>
2012-11-08 14:59:22 -0600

    Fix. Now the Nook sdcard will work for all mods instead of just one
    Whoops. Wasn't thinking. But this same method will fix the problems
    with CM7 pdroid as well, I believe. So this "Nook" specific script
    may not be long for the world.

--- patches/devices/encore/encore.sh
+++ patches/devices/encore/encore.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 
-echo ""
-echo "*** Hello Nook User ***"
-echo ""
-echo "At the prompt, enter 'sdcard' for sdcard installs"
-echo "Any other response will indicate EMMC"
-echo ""
-echo -n "Please type 'sdcard' if you are using an sdcard install: "
-read SDCARD
-        case $SDCARD in
-		[Ss][Dd]*)
-                        echo "---------------------------------------------------------------"
-                        echo " You have indicated you will be using an SDcard install"
-                        echo "" 
-                        echo "   The update.zip will be for SDcard installs only."
-                        echo ""
-                        echo " You will need to use an SDcard capable recovery"
-                        echo "        Look at Leapinlar's threads in"
-                        echo " XDA Nook Color General forum for help choosing proper recovery."
-                        echo "---------------------------------------------------------------"
-                        echo ""
-                        sleep 1
-                        echo -n "Damn fine device, if I might be permitted. "
-                        sleep 1
-                        echo "Damn fine."
-                        sleep 1
-                        echo ""
-                        echo "*** Encore.sh reply indicates an SDcard install" >> "$LOG"
-                        \rm -fr META-INF >> "$LOG"
-                        \cp -pr patches/devices/$DID/META-INF . >> "$LOG"
-			;;
-		*)
-                        echo "This update.zip will only work on EMMC installs"
-			;;
-	esac
+UPDIR=META-INF/com/google/android
+
+# Remove original updater-script
+\rm $UPDIR/updater-script >> "$LOG"
+
+# Move the template script in
+\cp -v patches/devices/$DID/$UPDIR/updater-script $UPDIR >> "$LOG"
+
+# add lines that were set with in the FILE list, from files.txt to UPDATE.txt
+echo "run_program(\"/sbin/busybox\", \"umount\", \"/system\");" >> UPDATE.txt
+
+# Append the list generated from files.txt to the new updater-script
+cat UPDATE.txt >> $UPDIR/updater-script
