17da1dc770dbe32901208ff4c7c5bcc19374ebb1
Mateor <mateorod9@gmail.com>
2013-08-27 14:52:08 -0500

    Allow params to be passed to run_split and split_opd.sh
    This makes it easy to split files for justa specific jar. run_split_and_move.sh
    just runs it for every OpenPDroid dir, and split_opd.sh cleans out unused files.
    
    When you add a param to either "framework2" say, it just kicks it down until
    it ends up as an argument to split_patch_opd.py.

--- ap_scripts/run_split_and_move.sh
+++ ap_scripts/run_split_and_move.sh
@@ -5,11 +5,20 @@
 # This will not solve/split the reservoir files, that either needs to be coded in or done by hand. Once done, it
 # should basically be done forever. This is useful as a garbage disposal, though.
 
+# This allows to pass arguments, so can split AND remove files at once.
+if [ $# -gt 0 ]; then
+     for FILE in $@; do
+          args+=($FILE)
+     done
+     args=( ${args[@]//*'run_split'*} )
+fi
+
+
 dirs=$(find patches/openpdroid/4.2 -name 2013*)
 dirs+=$(find patches/openpdroid/4.3 -name 2013*)
 
 for d in $dirs; do 
      cd ~/android/auto3/$d
-     ~/android/auto3/ap_scripts/split_opd.sh
+     ~/android/auto3/ap_scripts/split_opd.sh ${args[@]}
      #mv preloaded.patch framework_preloaded_preloaded.patch
 done
