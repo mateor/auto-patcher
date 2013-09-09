ca63706b7fcf3931cb48793bb77c89092455da5d
Mateor <mateorod9@gmail.com>
2013-09-09 11:22:04 -0500

    Add some fixes to Cygwin patch and adjust batch.sh
    @Caldair, this is going to make using some of those processing scripts a little
    different, sorry. I broke a part of the updater when I updated the batch.sh
    script, so I just reverted that change for now.
    
    The best way at the moment would be to go the directory with the whole patches
    in it, and call the split_mod.sh script from there, specifying its location.

--- batch.sh
+++ batch.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 
-source ap_scripts/environ_config.sh || source $(echo $0 | sed 's|\(.*\)/.*|\1|')/environ_config.sh
+abspath () {
+	case `uname -s` in
+	CYGWIN*)
+		echo $(cygpath -ua "$1") | sed 's:/$::g'
+		;;
+	Darwin)
+		#[[ $(echo $1 | awk '/^\//') == $1 ]] && echo "$1" || echo "$PWD/$1"
+		[[ ${1:0:1} == "/" ]] && echo "$1" || echo "$PWD/$1"
+		;;
+	Linux)
+                echo $(readlink -f "$1")
+		;;
+	*)
+		if [[ ${1:0:1} == "/" ]]; then
+			echo "$1"
+		elif [[ ${1:0:2} == "./" ]]; then
+			echo "$PWD/${1:2}"
+		else
+			echo "$PWD/$1"
+		fi
+		;;
+	esac
+}
+
+extpath () {
+	case `uname -s` in
+	CYGWIN*)
+		echo $(cygpath -da "$1")
+		;;
+	*)
+		echo $(abspath "$1")
+		;;
+	esac
+}
+ROOT=$(abspath `dirname "$0"`)
 
 BACKUPS=$(find . -name "*~")
 for backup in ${BACKUPS[@]}; do
@@ -14,3 +48,4 @@ cd "$ROOT"
 tar cvzf patch_patches.tgz patches
 
 zip autopatcher.zip auto_patcher patch_tools.tgz patch_patches.tgz ap_scripts/*
+
